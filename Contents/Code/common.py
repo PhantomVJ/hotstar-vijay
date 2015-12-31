69495c412462714da5b6f5953663ab9111c3c6fc
PhantomVJ <vijayan_r@yahoo.com>
2015-12-31 01:24:32 -0800

    Removed common.py

--- Contents/Code/common.py
+++
@@ -1,6 +0,0 @@
-################################################################################
-TITLE = "HSVijay"
-VERSION = '0.0' # Release notation (x.y - where x is major and y is minor)
-GITHUB_REPOSITORY = 'PhantomVJ/HSVijay.bundle'
-PREFIX = "/video/hsvijay"
-################################################################################
